f1c1f648ce6808b1047cd79ec5b289b39cb114b7
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2022-09-15 21:42:30 +0200

    Fix timelimit

--- 1721/c.py
+++ 1721/c.py
@@ -9,15 +9,14 @@ for _ in range(cases):
   b = list(map(int, input().split(" ")))
 
   # Minimumok: a[i] -> b[j<i], hogy b[j]>=a[i].
-  for i in range(n):
-    j = bisect_left(b, a[i], lo=0, hi=i+1)
-    print(b[j]-a[i], end=" ")
-  print()
+  print(" ".join(map(lambda i: str(b[bisect_left(b, a[i], lo=0, hi=i+1)]-a[i]), range(n))))
 
+  # Maximumok
+  mx = [0]*n
+  k = 0
   for i in range(n):
-    for k in range(i, n):
-      if k == n-1 or b[k] < a[k+1]:
-        break
-    print(b[k]-a[i], end=" ")
-  print()
-
+    k = max(k,i) # Don't restart search
+    while k<n-1 and b[k] >= a[k+1]:
+      k += 1
+    mx[i] = b[k]-a[i]
+  print(" ".join(map(str, mx)))
