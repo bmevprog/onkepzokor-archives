f90484f5a446111a88eb150ecd53fa2f671021a5
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2022-10-06 00:43:56 +0200

    Clean up bash script

--- run.sh
+++ run.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-set -e
+set -ex
 shopt -s globstar
+
+function process_params {
+  # Process options.
+  debug=false
+  while getopts ":hd" option; do
+    case $option in
+      h) echo "usage: $0 [-d] [source_file] ..."; exit ;;
+      d) debug=true ;;
+      ?) echo "error: option -$OPTARG is not implemented"; exit ;;
+    esac
+  done
+
+  # Remove the options from the positional parameters.
+  shift $((OPTIND-1))
+
+  source_file="$1"
+  extension="${source_file#*.}"
+}
 
 function compile_cpp {
   gpp_opts=()
@@ -8,41 +26,28 @@ function compile_cpp {
 
   out_file="$source_file".out
   g++ -o "$out_file" ${gpp_opts[@]} "$source_file"
-
-  executable="./$out_file"
 }
 
-function set_py_executable {
-  executable="python3 $source_file"
-}
-
-# Process options.
-debug=false
-while getopts ":hd" option; do
-  case $option in
-    h) echo "usage: $0 [-d] [source_file] ..."; exit ;;
-    d) debug=true ;;
-    ?) echo "error: option -$OPTARG is not implemented"; exit ;;
+function compile {
+  case $extension in
+    cpp) compile_cpp ;;
+    py) true ;;
+    ?) echo "error: extension $extension is not recognized"; exit ;;
   esac
-done
+}
 
-# Remove the options from the positional parameters.
-shift $(( OPTIND - 1 ))
+function evaluate_inputs {
+  dir="$(dirname "$source_file")"
+  for in_file in "$dir"/**/*.in; do
+    ans_file="${in_file%.*}".ans
+    echo $in_file ">>" $ans_file
+    case $extension in
+      cpp) ./"$out_file" < "$in_file" > "$ans_file" 2>&1 ;;
+      py) python3 $source_file < "$in_file" > "$ans_file" 2>&1 ;;
+    esac
+  done
+}
 
-source_file="$1"
-extension="${source_file#*.}"
-
-case $extension in
-  cpp) compile_cpp ;;
-  py) set_py_executable ;;
-  ?) echo "error: extension $extension is not recognized"; exit ;;
-esac
-
-# Run on all input files
-echo $executable
-dir="$(dirname "$source_file")"
-for in_file in "$dir"/**/*.in; do
-  ans_file="${in_file%.*}".ans
-  echo $in_file ">>" $ans_file
-  eval "$executable" < "${in_file}" > "${ans_file}"
-done
+process_params $@
+compile
+evaluate_inputs
