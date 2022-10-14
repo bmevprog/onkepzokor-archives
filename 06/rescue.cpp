5a887ae2788eda439c7267e7bb0b085d3a782f6e
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2022-10-14 12:28:24 +0200

    Simplify rescue.cpp: Prefix sum is easier when you add a 0 element at the beginning for padding.

--- 06/rescue.cpp
+++ 06/rescue.cpp
@@ -13,32 +13,20 @@ int main() {
   cin.tie(NULL);
 
   int n; cin>>n;
-  vector<int> a(n);
-  for(int i=0; i<n; ++i) {
+  vector<int> a(n+1);
+  for(int i=1; i<=n; ++i) {
     cin>>a[i];
   }
   vector<int> s(n);
-  s[0] = a[0];
-  for(int i=1; i<n; ++i) {
+  for(int i=1; i<=n; ++i) {
     s[i] = s[i-1] + a[i];
   }
 
-  if(a[0]%10 == 0) {
-    cout << 1 << " ";
-  } else {
-    for (int j = 1; j < n; ++j) {
-      if (s[j] % 10 == 0) {
-        cout << j + 1 << " ";
-        break;
-      }
-    }
-  }
- 
-  for(int i=1; i<n; ++i) {
+  for(int i=0; i<n; ++i) {
     bool voltMegoldas = false;
-    for(int j=i+1; j<n; ++j) {
-      if (s[i-1]%10 == s[j]%10) {
-        cout << j - i + 1 << " ";
+    for(int j=i+1; j<=n; ++j) {
+      if (s[i]%10 == s[j]%10) {
+        cout << j - i << " ";
         voltMegoldas = true;
         break;
       }
